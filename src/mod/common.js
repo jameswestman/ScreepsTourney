ee8ed6da0f31ed8f4e2005cbcf4d0ea493082587
FlyingPiMonster <kittycat31415@gmail.com>
2017-04-12 08:08:28 -0500

    Add constants from challenge to server constants

--- src/mod/common.js
+++ src/mod/common.js
@@ -2,8 +2,11 @@
 
 const fs = require('fs-promise')
 const webinterface = require('../webinterface')
+const _ = require('lodash');
 
 module.exports = function(config) {
     config.challenge = JSON.parse(fs.readFileSync("challenge.json", { encoding: "utf8" }))
     config.challengeAPI = new webinterface(config.challenge.interface, config.challenge.apikey)
+
+    _.extend(config.common.constants, config.challenge.rules.constants || {})
 }
