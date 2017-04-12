a94c1671861f00ddbb2c89ad260d2b7031dc78b0
FlyingPiMonster <kittycat31415@gmail.com>
2017-04-12 14:02:21 -0500

    Revoked Steam API key and replaced with argument

--- remakeServer.sh
+++ remakeServer.sh
@@ -2,7 +2,7 @@
 
 rm -r server
 mkdir server
-echo "B68796EDFE0F9AD94D01BEBF27F504E0" | screeps init server
+echo $1 | screeps init server
 npm run setup -- --location ./server --interface "http://internalapi.lvh.me:8080" --apikey "security"
 cd server
 screeps start
